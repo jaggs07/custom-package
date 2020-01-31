f3c858864a8e4996fe4c67975768942253d92416
jaggs07 <jagaran@fusemachines.com>
2020-01-31 11:29:01 +0545

    Custom package

---
+++ dist/components/Card/index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = Card;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function Card() {
+  return _react.default.createElement("div", {
+    className: "Card"
+  }, _react.default.createElement("div", {
+    className: "Card-Header"
+  }, "Card Title"), _react.default.createElement("div", {
+    className: "Card-body"
+  }, "Card Body"), _react.default.createElement("div", {
+    className: "Card-footer"
+  }, "Card Footer"));
+}
---
+++ src/components/Card/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Card () {
+  return <div className='Card'>
+   <div className='Card-Header'>
+      Card Title
+   </div>
+   <div className='Card-body'>
+       Card Body
+   </div>
+   <div className='Card-footer'>
+       Card Footer
+   </div>
+  </div>
+}
